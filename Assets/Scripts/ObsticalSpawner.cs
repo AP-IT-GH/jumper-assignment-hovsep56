64a128ab2f0e19b768d8347b332a5759c529f7de
blitzdawolf <botjes2@hotmail.com>
2021-12-10 00:56:23 +0100

    a

--- Assets/Scripts/ObsticalSpawner.cs
+++ Assets/Scripts/ObsticalSpawner.cs
@@ -3,18 +3,23 @@ using UnityEngine;
 public class ObsticalSpawner : MonoBehaviour
 {
     public GameObject prefab;
+    public float Change = 50;
     public Vector3 offset;
+    private float timer = 0;
 
     // Update is called once per frame
     void Update()
     {
-        if(Time.time % 2 == 0)
+        timer += Time.deltaTime;
+        if (timer > Random.Range(2, 20))
         {
-            if(Random.Range(0, 100) > 50)
+            if(Random.Range(0, 100) < Change)
             {
                 GameObject go = Instantiate(prefab, transform);
-                go.transform.localPosition = offset;
+                float u = (Random.Range(0, 100) > 50) ? 0 : 4;
+                go.transform.localPosition = offset + (Vector3.up * u);
             }
+            timer = 0;
         }
     }
 }
